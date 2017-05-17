983b5710823aa269e1ba7794b84c27ed542e357b
uyk <meopulior@gmail.com>
2017-05-18 01:13:08 +0900

    페이스북 게시시 facebook 메시지 출력

---
+++ index.js
@@ -0,0 +1,39 @@
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var request = require('request');
+
+var app = express();
+var port = 4000;
+
+var optionsSmartLamp = {
+	uri : "http://218.150.183.150:3000/pi3",
+	method : "GET",
+	headers : { "Content-Type" : "application/x-www-form-urlencoded"},
+}
+
+app.use(bodyParser.urlencoded( {extended : false } ));
+
+app.get('/', function(req,res) {
+	console.log('smart void lamp pi3');
+	res.send('Smart Voice Lamp pi3');
+});
+
+app.listen(port,function() {
+	console.log("create Server");
+
+	//1초마다 smartLamp 함수 호출
+	setInterval(smartLamp,1000);
+});
+
+function smartLamp() {
+	//GET 요청 전송
+	request(optionsSmartLamp, function(error, response, body) {
+		if(body == "empty" ) {
+			console.log("no data");
+		}
+		else {
+			console.log(body);
+		}
+	});
+}
